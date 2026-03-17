23935bfd0ce27fcdf54dda6bb385445a3c8403c0
je4nc4rlos0903-creator <je4nc4rlos0903@gmail.com>
2026-03-17 12:14:50 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,38 @@
+import mercadopago from "mercadopago";
 
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN
+});
+
+export default async function handler(req, res){
+
+  const body = req.body;
+
+  if(body.type === "payment"){
+
+    const payment = await mercadopago.payment.findById(body.data.id);
+
+    if(payment.body.status === "approved"){
+
+      const numeros = payment.body.metadata.numeros;
+
+      for(let num of numeros){
+        await fetch(
+          "https://qswajeisicdtsmmqokna.supabase.co/rest/v1/numeros_rifa?numero=eq."+num,
+          {
+            method:"PATCH",
+            headers:{
+              "Content-Type":"application/json",
+              apikey:"sb_publishable_7VLK-dHs3ybJKV2eBcS00Q_RzwvrmD2",
+              Authorization:"Bearer sb_publishable_7VLK-dHs3ybJKV2eBcS00Q_RzwvrmD2"
+            },
+            body: JSON.stringify({status:"pago"})
+          }
+        );
+      }
+
+    }
+  }
+
+  res.status(200).send("ok");
+}
