4a49730e5a6a110055281737fbbecbbda46230db
je4nc4rlos0903-creator <je4nc4rlos0903@gmail.com>
2026-03-17 12:55:22 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,13 +2,63 @@ export default async function handler(req, res){
 
   try {
 
-    console.log("Webhook:", req.body);
+    const body = req.body;
+
+    if(body.type === "payment"){
+
+      const paymentId = body.data?.id;
+
+      if(!paymentId){
+        return res.status(200).send("ok");
+      }
+
+      const response = await fetch(
+        `https://api.mercadopago.com/v1/payments/${paymentId}`,
+        {
+          headers:{
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+          }
+        }
+      );
+
+      const payment = await response.json();
+
+      if(payment.status === "approved"){
+
+        const numeros = payment.metadata?.numeros || [];
+        const nome = payment.metadata?.nome || "";
+
+        for(let num of numeros){
+
+          await fetch(
+            "https://qswajeisicdtsmmqokna.supabase.co/rest/v1/numeros_rifa?numero=eq."+num,
+            {
+              method:"PATCH",
+              headers:{
+                "Content-Type":"application/json",
+                apikey:"sb_publishable_7VLK-dHs3ybJKV2eBcS00Q_RzwvrmD2",
+                Authorization:"Bearer sb_publishable_7VLK-dHs3ybJKV2eBcS00Q_RzwvrmD2"
+              },
+              body: JSON.stringify({
+                status:"pago",
+                nome: nome
+              })
+            }
+          );
+
+        }
+
+      }
+
+    }
 
     res.status(200).send("ok");
 
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("erro");
+  } catch (error) {
+
+    console.error(error);
+    res.status(200).send("erro tratado");
+
   }
 
 }
